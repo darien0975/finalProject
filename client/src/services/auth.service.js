6b809aaa49d16066fb480f88de5fd9f9662002b0
Darien Chen <darien7557@fakeemail.com>
2024-06-01 01:09:37 +0800

    0601

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,30 +1,31 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/user";
+// const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://server-m178.onrender.com/api/user";
 
-class AuthService{
-    login(username , password){
-        return axios.post(API_URL + "/login" , {
-             username,
-             password,
-        });
-    }
-    logout(){
-        localStorage.removeItem("user");
-    }
-    register(username , name , password , contact , role , sex){
-        return axios.post(API_URL + "/register" ,{
-            username, 
-            name, 
-            password, 
-            contact,
-            role, 
-            sex,
-        });
-    }
+class AuthService {
+  login(username, password) {
+    return axios.post(API_URL + "/login", {
+      username,
+      password,
+    });
+  }
+  logout() {
+    localStorage.removeItem("user");
+  }
+  register(username, name, password, contact, role, sex) {
+    return axios.post(API_URL + "/register", {
+      username,
+      name,
+      password,
+      contact,
+      role,
+      sex,
+    });
+  }
 
-    getCurrentUser(){
-        return JSON.parse(localStorage.getItem("user"));
-    }
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
 }
 
 export default new AuthService();
